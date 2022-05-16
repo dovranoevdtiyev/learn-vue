8f868d50e34be89e7003fb808921b357a596cf23
dovranoevdtiyev <ovetdiyew02@gmail.com>
2022-05-17 00:02:11 +0500

    add new

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
+    <img src="" alt="" />
     <h1>This is an about page</h1>
-    <img src="/images/logo.png" alt="logo" />
+    <img src="./src/assets/apple.jpeg" alt="" />
   </div>
 </template>
