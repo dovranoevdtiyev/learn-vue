842eb40d60ad114c4b53cad77e8224ea0b5831b4
dovranoevdtiyev <ovetdiyew02@gmail.com>
2022-05-17 00:13:53 +0500

    added photo

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,6 +2,6 @@
   <div class="about">
     <img src="" alt="" />
     <h1>This is an about page</h1>
-    <img src="./src/assets/apple.jpeg" alt="" />
+    <img src="./assets/origin copy.webp" alt="" />
   </div>
 </template>
